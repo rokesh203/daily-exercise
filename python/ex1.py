3d567963654798a74ece60612c8e8a138202b845
rokesh203 <rokeshkrishnan203@gmail.com>
2025-07-18 22:03:35 +0530

    exercise

--- python/ex1.py
+++ python/ex1.py
@@ -1,8 +1,21 @@
 a=[]
-print("enter 10 number")
-for i in range(10):
+print("enter 4 number")
+for i in range(4):
     num = int(input("enter number:"+str(i+1) +":"))
     a.append(num)
     
-
 print(a)
+
+sum=0
+for i in a:
+    sum =sum+i
+    print(sum)
+
+
+
+
+"""sum = 0
+for i in range(1,):
+    sum += i
+    print(sum)
+    """
