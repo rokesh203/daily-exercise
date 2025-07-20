a69f688afa9a49caa8268d606a6fe516af9f621a
rokesh203 <rokeshkrishnan203@gmail.com>
2025-07-20 19:45:39 +0530

    exercise

--- python/ex1.py
+++ python/ex1.py
@@ -25,3 +25,10 @@ for i in range(5):
 for i in range(1,10):
     for j in range(1,6):
         print(j,"apple")
+        
+
+for i in range(1,3):
+    print("week :",i)
+    for j in range(1,4):
+        print("day :",j)
+
