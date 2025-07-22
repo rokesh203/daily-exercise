119d8e35624dd275eadbd9dc0ea8f25ff8941bc1
rokesh203 <rokeshkrishnan203@gmail.com>
2025-07-22 22:07:20 +0530

    exercise

--- python/function.py
+++ python/function.py
@@ -4,3 +4,12 @@ def additon():
     print(a+b)
     
 additon()
+
+def findeoro(a):
+    if(a%2==0):
+        print("even")
+    else:
+        print("odd")
+
+a=int(input("enter a number:"))
+findeoro(a)
