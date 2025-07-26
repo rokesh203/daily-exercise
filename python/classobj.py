db5cf06a371d69f5dd6c2914a29547e94b9e1bdb
rokesh203 <rokeshkrishnan203@gmail.com>
2025-07-26 22:10:55 +0530

    exercise

--- python/classobj.py
+++ python/classobj.py
@@ -1,4 +1,4 @@
-class vinoth:
+"""class vinoth:
     drink=""
     def read(self):
         print("aptitude")
@@ -11,4 +11,17 @@ rokesh =vinoth()
 gowri = vinoth()
 
 rokesh.phone()
-gowri.read()
+gowri.read()"""
+
+class student:
+    def __init__(self):
+        name=""
+        register=""
+    def display(self):
+        print("name",self.name)
+        print("register",self.register)
+        
+stu = student()
+stu.name="rokesh"
+stu.register=123
+stu.display()
