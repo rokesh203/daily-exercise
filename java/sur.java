72996b405d09f86de8cd845b4386f2a7b21736f2
rokesh203 <rokeshkrishnan203@gmail.com>
2025-07-29 22:33:24 +0530

    exercise

---
+++ java/sur.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sur{
+    public static void main(String args[]){
+        Scanner s= new Scanner(System.in);
+        String name=s.nextLine();
+        if(name.equals("dead"))
+        System.out.println("surya is dead");
+        else
+        System.out.println("surya weds");
+    }
+}
