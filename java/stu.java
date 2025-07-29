34c0d723fef1e7b9d5f23ae9c5f070390e521075
rokesh203 <rokeshkrishnan203@gmail.com>
2025-07-29 21:06:45 +0530

    exercise

---
+++ java/stu.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class stu{
+    public static void main(String args[]){
+        Scanner s= new Scanner(System.in);
+        String name=s.nextLine();
+        int age=s.nextInt();
+        s.nextLine();
+        String address=s.nextLine();
+        System.out.println("name"+name);
+        System.out.println(age);
+        System.out.println(address);
+
+    }
+}
