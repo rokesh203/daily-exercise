eeff636d92d860d18b899cfe91eb73dd16d76882
rokesh203 <rokeshkrishnan203@gmail.com>
2025-07-30 21:17:26 +0530

    exercise

---
+++ java/Exer.java
@@ -0,0 +1,13 @@
+class Exer{
+    public static void main(String args[]){
+        int count=0;
+        for(int i=0;i<=100;i++){
+            if(i%3==0&&i%5 == 0){
+                System.out.println(i);
+            }
+                    count=count+1;
+        System.out.println(count);
+        }
+
+    }
+}
