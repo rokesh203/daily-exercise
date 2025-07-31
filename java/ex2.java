bee9e426c4df39518ecef3e2137c45dfa926a2d5
rokesh203 <rokeshkrishnan203@gmail.com>
2025-07-31 21:06:57 +0530

    exercise

---
+++ java/ex2.java
@@ -0,0 +1,17 @@
+public class ex2 {
+    public static void main(String args[]){
+        for(int i=1;i<=3;i=i+1){
+            System.out.println("selva");
+            for(int j=1;j<=2;j=j+1){
+                System.out.println("rokesh");
+                for(int k=1;k<=3;k++){
+                    System.out.println("vinoth");
+                    for(int l=1;l<=2;l++){
+                        System.out.println("gowri");
+                    }
+                }
+            }
+        }
+    }
+    
+}
